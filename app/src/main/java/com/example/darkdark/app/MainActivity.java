0c4eb564a39c046d0083471bab7b41e84369fd06
bobbyw8 <bobbyw8@hawaii.edu>
2015-10-29 20:40:07 -1000

    moved step stuff to fragment and made ugly colors for the background to check if the tablayout works. Also formed basic fragments,tabs, etc. for integrity and future edits.
    also i broke the total steps since reboot thing, fix it in SecFragment.
    
    also there are TODO things from using create new fragment. >.<

--- app/src/main/java/com/example/darkdark/app/MainActivity.java
+++ app/src/main/java/com/example/darkdark/app/MainActivity.java
@@ -1,58 +1,126 @@
 package com.example.darkdark.app;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.hardware.*;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v4.app.Fragment;
+
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.Toolbar;
+
+
+import org.w3c.dom.Text;
 
 import java.io.Console;
 
 
-public class MainActivity extends FragmentActivity implements SensorEventListener {
-    private TextView textCount;
-    private TextView textTotal;
-    private SensorManager mSensorManager;
-    private Sensor mStepCounterSensor;
-    private Sensor mStepDetectorSensor;
+public class MainActivity extends AppCompatActivity/*FragmentActivity*/ implements SecFragment.OnFragmentInteractionListener,
+                                                                                SecondFragment.OnFragmentInteractionListener,
+                                                                                ThirdFragment.OnFragmentInteractionListener {
+
+//    private TextView textCount;
+//    private TextView textTotal;
+    public SensorManager mSensorManager;
+    public Sensor mStepCounterSensor;
+    public Sensor mStepDetectorSensor;
     private int steps = -1;
     private String PREV_S;
+
+    // 10/29 --->
+    boolean activityRunning;
+    private Toolbar mToolbar; //v7 widget supports older devices
+    private TabLayout mTabLayout;
+    private ViewPager mPager;
+    private MyPagerAdapter mAdapter;
+
+
+    // <---- 10/29
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         if (savedInstanceState != null)
         {
             steps = savedInstanceState.getInt(PREV_S) - 1;
         }
-        setContentView(R.layout.activity_main);
-        textCount = (TextView) findViewById(R.id.count);
-        textTotal = (TextView) findViewById(R.id.total);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        setContentView(R.layout.fragment_main);//changed from activity_main cause it was just calling fragment_main anyway
+
+        mToolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(mToolbar);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+        mTabLayout.setTabsFromPagerAdapter(mAdapter);
+        mTabLayout.setupWithViewPager(mPager);
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+//        mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        //fragment manager stuff to check if the fragment worked i guess,
+        //this is unnecessary when implementing viewpager. since its taken care of.
+//         SecFragment frag = new SecFragment();
+//         FragmentManager manager= getSupportFragmentManager();
+//         android.support.v4.app.FragmentTransaction transaction= manager.beginTransaction();
+//         transaction.add(R.id.mainthing,frag,"MainFragment");
+//         transaction.commit();
+
+        // <---
+        //the shit below needs to go below this fragment manager stuff FUCK, give me back all of my time T_T
+
+//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+//        mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+//       textCount = (TextView) findViewById(R.id.count);
+//        textTotal = (TextView) findViewById(R.id.total);
+
+
+
+
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mStepCounterSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_FASTEST);
+//        activityRunning = true;
+//        Sensor countSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+//        if (countSensor != null) {
+//            mSensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+//        }
+//        //mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_FASTEST);
+//        else {
+//            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+//        }
     }
-
     @Override
-    protected void onStop()
+    protected void onPause()
     {
-        super.onStop();
-        mSensorManager.unregisterListener(this, mStepCounterSensor);
-        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+        super.onPause();
+       // activityRunning = false;
     }
 
     @Override
@@ -78,31 +146,110 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
         return super.onOptionsItemSelected(item);
     }
 
-    public void onSensorChanged(SensorEvent event)
-    {
-        Sensor sensor = event.sensor;
-        float[] values = event.values;
-        int value = -1;
-        if (values.length > 0)
-        {
-            steps++;
-            value = (int) values[0];
-        }
-        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER)
-        {
-            textCount.setText("" + steps);
-            textTotal.setText("" + value);
-
-        }
-    }
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy)
-    {
-
-    }
-    @Override
+//    public void onSensorChanged(SensorEvent event)
+//    {
+//        /*Sensor sensor = event.sensor;
+//        float[] values = event.values;
+//        int value = -1;
+//        if (values.length > 0)
+//        {
+//            steps++;
+//            value = (int) values[0];
+//        }
+//        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER)
+//        {
+//            textCount.setText("" + steps);
+//            textTotal.setText("" + value);
+//
+//        }*/
+//        if(activityRunning){
+//
+//
+//                textCount.setText(String.valueOf((int) event.values[0]));
+//                //added (int) so no decimal
+//
+//
+//        }
+//
+//    }
+//    @Override
+//    public void onAccuracyChanged(Sensor sensor, int accuracy)
+//    {
+//
+//    }
+//    @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt(PREV_S, steps);
         super.onSaveInstanceState(savedInstanceState);
     }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    //
+//    public static class MyFragment extends Fragment{
+//        public MyFragment(){
+//
+//        }
+//
+//        //public static MyFragment newInstance(int pageN)
+//
+//        public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState){
+//            View myFragmentView = inflater.inflate(R.layout.counter_info,container,false);
+//
+//            return myFragmentView;
+//        }
+//    }
+
+
+
+    //For TABLAYOUT CRAP
+    class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        //gives position and ask to return fragment at that position
+        {
+            switch(position) {
+
+                case 0:
+                    return SecFragment.newInstance("FirstFragment", "1");
+                case 1:
+                    return SecondFragment.newInstance("2ndFragment", "2");
+                case 2:
+                    return ThirdFragment.newInstance("3rdFragment", "3");
+                default:
+                    return SecFragment.newInstance("FirstFragment", "1");
+            }
+            //return fragment at the position
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        public CharSequence getPageTitle(int position){
+
+            switch(position){
+                case 0: return ""+position;
+                case 1: return ""+position;
+                case 2: return ""+position;
+                    default: return "0";
+            }
+
+        }
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/darkdark/app/SecFragment.java
@@ -0,0 +1,181 @@
+package com.example.darkdark.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Bundle;
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SecFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SecFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SecFragment extends Fragment implements SensorEventListener{
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private TextView textCount;
+    private TextView textTotal;
+    private SensorManager mSensorManager;
+    private Sensor mStepCounterSensor;
+    private Sensor mStepDetectorSensor;
+    boolean activityRunning;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SecFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SecFragment newInstance(String param1, String param2) {
+        SecFragment fragment = new SecFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SecFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sec,
+                container, false);
+        textCount = (TextView) view.findViewById(R.id.count);
+        textTotal = (TextView) view.findViewById(R.id.total);
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        try {
+            mListener = (OnFragmentInteractionListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getActivity().toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(activityRunning){
+
+
+            textCount.setText(String.valueOf((int) event.values[0]));
+            //added (int) so no decimal
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        activityRunning = true;
+        Sensor countSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            mSensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+        //mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        else {
+            Toast.makeText(getActivity(),"Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        activityRunning=false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+
+
+
+}
