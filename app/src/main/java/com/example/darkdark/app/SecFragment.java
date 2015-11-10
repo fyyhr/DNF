f3d00b70e79810e18159a2f509b6fb1f888a007f
bobbyw8 <bobbyw8@hawaii.edu>
2015-11-09 17:06:13 -1000

    save and time

--- app/src/main/java/com/example/darkdark/app/SecFragment.java
+++ app/src/main/java/com/example/darkdark/app/SecFragment.java
@@ -11,8 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 //import android.app.Fragment;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
