7e2ae08179caec234f342706b09b45943fa8dd55
fyyhr <shiftlocksuckz@gmail.com>
2015-11-28 20:44:37 -1000

    + splash screen for xhdpi (640x960)  320dpi + splash screen fade out animation into app

---
+++ app/src/main/java/com/example/darkdark/app/splash.java
@@ -0,0 +1,38 @@
+package com.example.darkdark.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(splash.this,MainActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.fin, R.anim.fout);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
